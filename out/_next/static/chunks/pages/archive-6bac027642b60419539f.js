75d1bd15e0bd536a6723fe9854ab483981236fd4
Rahul kumar <rkt82224@gmail.com>
2021-07-16 13:23:07 +0530

    build is added

---
+++ out/_next/static/chunks/pages/archive-6bac027642b60419539f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{1031:function(n,u,r){"use strict";function t(){return"Archive"}r.r(u),r.d(u,{default:function(){return t}})},3274:function(n,u,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/archive",function(){return r(1031)}])}},function(n){n.O(0,[774,888,179],(function(){return u=3274,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
